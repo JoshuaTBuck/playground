ae3e9e458a3f70d8613e4baefbb1c2654ea8a579
JoshuaBuck <joshua-tristan.buck@fielmann.com>
2023-02-19 18:17:25 +0100

    sql backend tests

---
+++ backend/src/main/java/playground/backend/BlogController.java
@@ -0,0 +1,55 @@
+package playground.backend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class BlogController {
+
+    @Autowired
+    BlogRespository blogRespository;
+
+    @GetMapping("/blogs")
+    public List<Blog> index() {
+        return blogRespository.findAll();
+    }
+
+    @GetMapping("/blog/{id}")
+    public Blog show(@PathVariable String id) {
+        Integer blogId = Integer.parseInt(id);
+        return blogRespository.findById(blogId).get();
+    }
+
+    @PostMapping("/blog/search")
+    public List<Blog> search(@RequestBody Map<String, String> body) {
+        String searchTerm = body.get("text");
+        return blogRespository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
+    }
+
+    @PostMapping("/blog")
+    public Blog create(@RequestBody Map<String, String> body) {
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogRespository.save(new Blog(title, content));
+    }
+
+    @PutMapping("/blog/{id}")
+    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body) {
+        Integer blogId = Integer.parseInt(id);
+        // getting blog
+        Blog blog = blogRespository.findById(blogId).get();
+        blog.setTitle(body.get("title"));
+        blog.setContent(body.get("content"));
+        return blogRespository.save(blog);
+    }
+
+    @DeleteMapping("blog/{id}")
+    public boolean delete(@PathVariable String id) {
+        int blogId = Integer.parseInt(id);
+        blogRespository.deleteById(blogId);
+        return true;
+    }
+}
